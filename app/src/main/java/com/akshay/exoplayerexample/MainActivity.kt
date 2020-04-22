34647424ab0f1b83052211a299ad519c0df08208
anandwana001 <akshaynandwana001@gmail.com>
2020-04-22 13:08:57 +0530

    added project

---
+++ app/src/main/java/com/akshay/exoplayerexample/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.akshay.exoplayerexample
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var simpleExoPlayer: SimpleExoPlayer? = null
+    private val mp3Url = "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lifecycle.addObserver(ExoplayerObserver())
+    }
+
+    private fun initializeExoPlayer() {
+        simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(this)
+        simpleExoPlayer?.let {
+            playerView.player = simpleExoPlayer
+            val mediaSource = buildMediaSource()
+            it.setPlayWhenReady(playWhenReady)
+            it.seekTo(currentWindow, playbackPosition)
+            it.prepare(mediaSource, false, false)
+        }
+    }
+
+    private fun buildMediaSource(): MediaSource {
+        val dataSourceFactory = DefaultDataSourceFactory(this, "sample_exoplayer")
+        return ProgressiveMediaSource.Factory(
+                dataSourceFactory
+        ).createMediaSource(Uri.parse(mp3Url))
+    }
+
+    private fun hideSystemUi() {
+        playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+    private fun releasePlayer() {
+        simpleExoPlayer?.let {
+            playWhenReady = it.getPlayWhenReady()
+            playbackPosition = it.getCurrentPosition()
+            currentWindow = it.getCurrentWindowIndex()
+            it.release()
+            simpleExoPlayer = null
+        }
+    }
+
+    inner class ExoplayerObserver : LifecycleObserver {
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        fun onResume() {
+            if ((Util.SDK_INT < 24 || simpleExoPlayer == null)) {
+                initializeExoPlayer()
+            }
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_START)
+        fun onStart() {
+            if (Util.SDK_INT >= 24) {
+                initializeExoPlayer()
+            }
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+        fun onStop() {
+            if (Util.SDK_INT >= 24) {
+                releasePlayer()
+            }
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        fun onPause() {
+            if (Util.SDK_INT >= 24) {
+                releasePlayer()
+            }
+        }
+
+    }
+
+}
