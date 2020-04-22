1170c8d4c8380642482dd212f16e27c9e541904a
anandwana001 <akshaynandwana001@gmail.com>
2020-04-22 17:05:57 +0530

    added listener

--- app/src/main/java/com/akshay/exoplayerexample/MainActivity.kt
+++ app/src/main/java/com/akshay/exoplayerexample/MainActivity.kt
@@ -2,12 +2,15 @@ package com.akshay.exoplayerexample
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
@@ -18,16 +21,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private var simpleExoPlayer: SimpleExoPlayer? = null
-    private val mp3Url = "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
+    private val mp3Url = "https://storage.googleapis.com/exoplayer-test-media-0/Jazz_In_Paris.mp3"
+    private val mp4Url =
+        "https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4"
+    private val dashUrl =
+        "https://www.youtube.com/api/manifest/dash/id/bf5bb2419360daf1/source/youtube?as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams=ip,ipbits,expire,source,id,as&ip=0.0.0.0&ipbits=0&expire=19000000000&signature=51AF5F39AB0CEC3E5497CD9C900EBFEAECCCB5C7.8506521BFC350652163895D4C26DEE124209AA9E&key=ik0"
     private var playWhenReady = true
     private var currentWindow = 0
     private var playbackPosition: Long = 0
+
+    private lateinit var playbackStateListener: PlaybackStateListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         lifecycle.addObserver(ExoplayerObserver())
+        playbackStateListener = PlaybackStateListener()
     }
 
     private fun initializeExoPlayer() {
@@ -35,6 +45,7 @@ class MainActivity : AppCompatActivity() {
         simpleExoPlayer?.let {
             playerView.player = simpleExoPlayer
             val mediaSource = buildMediaSource()
+            it.addListener(playbackStateListener)
             it.setPlayWhenReady(playWhenReady)
             it.seekTo(currentWindow, playbackPosition)
             it.prepare(mediaSource, false, false)
@@ -44,7 +55,7 @@ class MainActivity : AppCompatActivity() {
     private fun buildMediaSource(): MediaSource {
         val dataSourceFactory = DefaultDataSourceFactory(this, "sample_exoplayer")
         return ProgressiveMediaSource.Factory(
-                dataSourceFactory
+            dataSourceFactory
         ).createMediaSource(Uri.parse(mp3Url))
     }
 
@@ -59,11 +70,40 @@ class MainActivity : AppCompatActivity() {
 
     private fun releasePlayer() {
         simpleExoPlayer?.let {
+            it.removeListener(playbackStateListener)
             playWhenReady = it.getPlayWhenReady()
             playbackPosition = it.getCurrentPosition()
             currentWindow = it.getCurrentWindowIndex()
             it.release()
             simpleExoPlayer = null
+        }
+    }
+
+    private class PlaybackStateListener : Player.EventListener {
+
+        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            lateinit var exoPlayerState: String
+            when (playbackState) {
+                ExoPlayer.STATE_IDLE -> {
+                    exoPlayerState = "STATE_IDLE"
+                }
+                ExoPlayer.STATE_BUFFERING -> {
+                    exoPlayerState = "STATE_BUFFERING"
+                }
+                ExoPlayer.STATE_READY -> {
+                    exoPlayerState = "STATE_READY"
+                }
+                ExoPlayer.STATE_ENDED -> {
+                    exoPlayerState = "STATE_ENDED"
+                }
+                else -> {
+                    exoPlayerState = "UNKNOWN"
+                }
+            }
+            Log.d(
+                "ExoPlayerLog",
+                "changed state to ${exoPlayerState} playWhenReady: ${playWhenReady}"
+            )
         }
     }
 
